cfe02abc778b559e71e688e6d8aec37ee6b8df00
Unknown <aakashbanik510@gmail.com>
2019-02-05 11:21:53 +0530

    Basic Firebase Put Code
    Using a simple DHT sensor at first

---
+++ dht-firebase.py
@@ -0,0 +1,61 @@
+
+import RPi.GPIO as GPIO
+from time import sleep
+import datetime
+from firebase import firebase
+import Adafruit_DHT
+
+import urllib2, urllib, httplib
+import json
+import os 
+from functools import partial
+
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(False)
+
+sensor = Adafruit_DHT.DHT11
+
+pin = 4
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+
+firebase = firebase.FirebaseApplication('https://my-pi-12.firebaseio.com/', None)
+
+def get_temp():
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	if humidity is not None and temperature is not None:
+		sleep(5)
+		str_temp = ' {0:0.2f} *C '.format(temperature)	
+		str_hum  = ' {0:0.2f} %'.format(humidity)
+		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))	
+			
+	else:
+		print('Failed to get reading. Try again!')	
+		sleep(10)
+	
+	return temperature, humidity
+
+
+def update_firebase():
+
+	temp, hum = get_temp()
+
+	data = {"temp": temp, "humidity": hum}
+	firebase.post('/sensor/dht', data)
+	
+
+while True:
+		update_firebase()
+        #sleepTime = int(sleepTime)
+		sleep(5)
+	
+
+
+
+
+
+
+
+
